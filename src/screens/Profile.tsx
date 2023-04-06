2335afcb90435603b1ca8de86aaaef56cc7e7469
Gustavoph <gusta.coliveira07@gmail.com>
2023-04-06 15:03:46 -0300

    feat: add ImagePicker

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -2,21 +2,58 @@ import { Button } from '@components/Button'
 import { Input } from '@components/Input'
 import { ScreenHeader } from '@components/ScreenHeader'
 import { UserPhoto } from '@components/UserPhoto'
+import * as FileSystem from 'expo-file-system'
 import {
   Center,
   ScrollView,
   VStack,
   Skeleton,
   Text,
+  useToast,
   Heading,
 } from 'native-base'
+import * as ImagePicker from 'expo-image-picker'
 import { useState } from 'react'
 import { TouchableOpacity } from 'react-native'
 
 const PHOTO_SIZE = 33
 
 export function Profile() {
-  const [photoIsLoading] = useState(false)
+  const [photoIsLoading, setPhotoIsLoading] = useState(false)
+  const [userPhoto, setUserPhoto] = useState('https://github.com/gustavoph.png')
+  const toast = useToast()
+
+  async function handleUserPhotoSelect() {
+    try {
+      setPhotoIsLoading(true)
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      })
+
+      if (!photoSelected.canceled && photoSelected.assets[0].uri) {
+        const photoInfo = await FileSystem.getInfoAsync(
+          photoSelected.assets[0].uri,
+        )
+
+        if (photoInfo.exists && photoInfo.size / 1024 / 1024 > 5) {
+          return toast.show({
+            title: 'Essa imagem é muito grande, escolha uma de até 5MB',
+            placement: 'top',
+            bgColor: 'red.500',
+          })
+        }
+
+        setUserPhoto(photoSelected.assets[0].uri)
+      }
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setPhotoIsLoading(false)
+    }
+  }
 
   return (
     <VStack flex={1}>
@@ -34,13 +71,13 @@ export function Profile() {
             />
           ) : (
             <UserPhoto
-              source={{ uri: 'https://github.com/gustavoph.png' }}
+              source={{ uri: userPhoto }}
               alt="Foto do usuário"
               size={PHOTO_SIZE}
             />
           )}
 
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleUserPhotoSelect}>
             <Text
               color="green.500"
               fontWeight="bold"
@@ -56,7 +93,14 @@ export function Profile() {
 
           <Input bg="gray.600" placeholder="E-mail" isDisabled />
 
-          <Heading color="gray.200" fontSize="md" mb={2}>
+          <Heading
+            color="gray.200"
+            fontSize="md"
+            mb={2}
+            alignSelf="flex-start"
+            mt={12}
+            fontFamily="heading"
+          >
             Alterar senha
           </Heading>
 
