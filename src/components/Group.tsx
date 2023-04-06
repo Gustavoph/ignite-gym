ead98f4a6aa99272d0c5a34700301d01d7247549
Gustavoph <gusta.coliveira07@gmail.com>
2023-04-06 14:11:56 -0300

    feat: finish screens

--- src/components/Group.tsx
+++ src/components/Group.tsx
@@ -1,13 +1,35 @@
-import { Text } from 'native-base'
+import { Pressable, Text, IPressableProps } from 'native-base'
 
-type Props = {
+type Props = IPressableProps & {
   name: string
+  isActive?: boolean
 }
 
-export function Group({ name }: Props) {
+export function Group({ name, isActive = false, ...rest }: Props) {
   return (
-    <Text color="gray.200" fontSize="xs" fontWeight="bold">
-      {name.toUpperCase()}
-    </Text>
+    <Pressable
+      mr={3}
+      w={24}
+      h={10}
+      bg="gray.600"
+      rounded="md"
+      justifyContent="center"
+      alignItems="center"
+      overflow="hidden"
+      isPressed={isActive}
+      _pressed={{
+        borderColor: 'green.500',
+        borderWidth: 1,
+      }}
+      {...rest}
+    >
+      <Text
+        color={isActive ? 'green.500' : 'gray.200'}
+        fontSize="xs"
+        fontWeight="bold"
+      >
+        {name.toUpperCase()}
+      </Text>
+    </Pressable>
   )
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,12 +1,80 @@
+import { ExerciseCard } from '@components/ExerciseCard'
 import { Group } from '@components/Group'
 import { HomeHeader } from '@components/HomeHeader'
-import { VStack } from 'native-base'
+import { useNavigation } from '@react-navigation/native'
+import { AppNavigatorRoutesProps } from '@routes/app.routes'
+import { VStack, FlatList, HStack, Heading, Text } from 'native-base'
+import { useState } from 'react'
 
 export function Home() {
+  const [groups] = useState(['Costas', 'Bíceps', 'Tríceps', 'Ombro', 'Pernas'])
+  const [exercises] = useState([
+    'Puxada frontal',
+    'Remada curvada',
+    'Remada unilateral',
+    'Levantamento terras',
+  ])
+  const [groupSelected, setGroupSelected] = useState('Costas')
+
+  function handleChangeGroup(group: string) {
+    setGroupSelected(group)
+  }
+
+  const navigation = useNavigation<AppNavigatorRoutesProps>()
+
+  function handleOpenExerciseDetails() {
+    navigation.navigate('exercise')
+  }
+
   return (
     <VStack flex={1}>
       <HomeHeader />
-      <Group name="Costas" />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={
+              groupSelected.toLocaleUpperCase() === item.toLocaleUpperCase()
+            }
+            onPress={() => handleChangeGroup(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{
+          px: 8,
+        }}
+        my={10}
+        maxH={10}
+        minH={10}
+      />
+
+      <VStack px={8}>
+        <HStack justifyContent="space-between" mb={5}>
+          <Heading color="gray.200" fontSize="md" fontFamily="heading">
+            Exercícios
+          </Heading>
+
+          <Text color="gray.200" fontSize="sm">
+            {exercises.length}
+          </Text>
+        </HStack>
+
+        <FlatList
+          data={exercises}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <ExerciseCard onPress={handleOpenExerciseDetails} />
+          )}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{
+            paddingBottom: 20,
+          }}
+        />
+      </VStack>
     </VStack>
   )
 }
